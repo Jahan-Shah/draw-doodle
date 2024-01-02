ed2d36dcf41fe0872fc8b5b76ab745bbae874449
Shahjahan Sarfraz <110630702+Jahan-Shah@users.noreply.github.com>
2024-01-02 19:32:31 +0500

    :sparkles: Added class names for lookup

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
 import { Button } from "@/components/ui/button";
 import Canvas from "@/components/Canvas.vue";
 import Video from "@/components/videoCanvas.vue";
 import { ref } from "vue";
+import classes from "@/assets/class_names.json";
 
 const mode = ref(true);
 
@@ -12,11 +18,21 @@ const toggleMode = () => {
 </script>
 
 <template>
-  <Button
-    @click="toggleMode"
-    class="absolute z-10 top-4 left-1/2 -translate-x-1/2"
-    >Switch to {{ mode ? "Video" : "Canvas" }}</Button
+  <div
+    class="absolute flex items-center gap-3 z-10 top-4 left-1/2 -translate-x-1/2"
   >
+    <Button @click="toggleMode"
+      >Switch to {{ mode ? "Video" : "Canvas" }}</Button
+    >
+    <HoverCard>
+      <HoverCardTrigger>Classes</HoverCardTrigger>
+      <HoverCardContent class="w-max">
+        <div class="grid grid-cols-3 gap-4">
+          <p v-for="cls in classes">{{ cls }}</p>
+        </div>
+      </HoverCardContent>
+    </HoverCard>
+  </div>
   <main class="flex relative p-4 items-center justify-center h-[100vh]">
     <Canvas v-if="mode" />
     <Video v-else />
